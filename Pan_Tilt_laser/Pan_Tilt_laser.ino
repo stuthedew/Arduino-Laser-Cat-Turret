1ec69eadba7c80eb02cf662774db31b96978f435
Stuart Feichtinger <user.email "stuart.feichtinger@gmail.com>
2014-10-14 23:14:16 -0500

    Fixed typo in rollover handling

--- Pan_Tilt_laser/Pan_Tilt_laser.ino
+++ Pan_Tilt_laser/Pan_Tilt_laser.ino
@@ -206,7 +206,7 @@ void sleep(unsigned int minTime, unsigned int maxTime){
     while(millis() - startTime < 60000){
       heartBeat();
       delay(10000);
-      if(startTime > millis){ //check for rollovers
+      if(startTime > millis()){ //check for rollovers
         break;
       }
     }
